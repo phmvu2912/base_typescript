6ed9c77cd7f7c593b08ea5f4839eb18137128bed
phmvu2912 <phmvu2912@gmail.com>
2024-03-04 23:59:31 +0700

    update 4/3/24

---
+++ labs/lab1/lab1..ts
@@ -0,0 +1,8 @@
+// Bài 2: Tạo HelloWorld với Typescript
+console.log("Hello world");
+
+// Bài 3: Viết phương trình tính tổng 2 số
+const a: number = 10;
+const b: number = 20;
+
+console.log(a + b);
