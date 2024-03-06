39a2a950fcbdf0e7de6c13051f82e433b11a9c69
phmvu2912 <phmvu2912@gmail.com>
2024-03-06 13:14:21 +0700

    updata 6/3/24

---
+++ js/labs/lab1/lab1..js
@@ -0,0 +1,7 @@
+"use strict";
+// Bài 2: Tạo HelloWorld với Typescript
+console.log("Hello world");
+// Bài 3: Viết phương trình tính tổng 2 số
+const a = 10;
+const b = 20;
+console.log(a + b);
---
+++ js/lession2/index.js
@@ -0,0 +1,12 @@
+"use strict";
+{
+    const myInfo = {
+        name: "Pham Dao Vu",
+        age: 20,
+        email: "phmvu2912@gmail.com",
+        address: "Hanoi",
+        isStudent: true,
+        skills: ["HTML", "CSS", "JS"],
+    };
+    console.log(myInfo.skills);
+}
